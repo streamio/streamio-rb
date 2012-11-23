350e769d3d8f5fc0d44da856eda60e2789e5594c
David Backeus <duztdruid@gmail.com>
2012-11-23 08:32:43 +0100

    Allow focus specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,11 @@ STREAMIO_PASSWORD = "password"
 
 require 'webmock/rspec'
 
+RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+end
+
 def fixture_path
   @fixture_path ||= File.join(File.dirname(__FILE__), 'fixtures')
 end
